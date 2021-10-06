64610b5b90dac15a80eeb3f2c4052da564591824
Floor Loogman <floogman@mi-c1-p2.s19.be>
2021-10-06 10:56:21 +0200

    finished gnl tester

--- get_next_line/tests/main.c
+++ get_next_line/tests/main.c
@@ -6,12 +6,13 @@
 /*   By: floogman <floogman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 13:17:35 by floogman          #+#    #+#             */
-/*   Updated: 2021/09/29 13:04:17 by floogman         ###   ########.fr       */
+/*   Updated: 2021/10/06 10:37:27 by floogman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../get_next_line.h"
-#include <fcntl.h>
+#include <fcntl.h> // open
+#include <string.h> // strncmp
 #include <stdio.h>
 
 int	main(int argc, char **argv)
@@ -20,17 +21,30 @@ int	main(int argc, char **argv)
 	char	*line;
 
 	if (argc == 1)
-		fd = 0;
-	else if (argc == 2)
-		fd = open(argv[1], O_RDONLY);
-	else
+	{
+		while (get_next_line(0, &line) == 1)
+		{
+			if (line && !strncmp(line, "exit", 4))
+				break ;
+			printf("%s\n", line);
+			free(line);
+		}
+		return (0);
+	}
+	else if (argc != 2)
 		return (-1);
+	fd = open(argv[1], O_RDONLY);
 	while (get_next_line(fd, &line) == 1)
 	{
 		printf("%s\n", line);
 		free(line);
 	}
-	if (argc == 2)
-		close(fd);
+	printf("%s", line);
+	/* ~ for easier output comparison ~ */
+	if (line && !strncmp(line, "", 1))
+		printf("\n");
+	/* ~~ with a buffer size of zero ~~ */
+	free(line);
+	close(fd);
 	return (0);
 }
--- get_next_line/tests/main_bonus.c
+++ get_next_line/tests/main_bonus.c
@@ -6,31 +6,54 @@
 /*   By: floogman <floogman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 13:17:35 by floogman          #+#    #+#             */
-/*   Updated: 2021/09/29 13:04:21 by floogman         ###   ########.fr       */
+/*   Updated: 2021/10/06 10:37:24 by floogman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../bonus/get_next_line_bonus.h"
-#include <fcntl.h>
+#include <fcntl.h> // open
+#include <string.h> // strncmp
 #include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-	int		fd;
+	int		fd1;
+	int		fd2;
+	int		tmp;
+	int		ret;
+	int		count;
 	char	*line;
 
 	if (argc == 1)
-		fd = 0;
-	else if (argc == 2)
-		fd = open(argv[1], O_RDONLY);
-	else
+	{
+		while (get_next_line(0, &line) == 1)
+		{
+			if (line && !strncmp(line, "exit", 4))
+				break ;
+			printf("%s\n", line);
+			free(line);
+		}
+		return (0);
+	}
+	else if (argc != 3)
 		return (-1);
-	while (get_next_line(fd, &line) == 1)
+	fd1 = open(argv[1], O_RDONLY);
+	fd2 = open(argv[2], O_RDONLY);
+	count = 0;
+	tmp = fd1;
+	while ((ret = get_next_line(tmp, &line)) != -1)
 	{
 		printf("%s\n", line);
 		free(line);
+		tmp = fd2;
+		if (count++ % 2)
+			tmp = fd1;
+		if (!ret && !get_next_line(tmp, &line))
+			break;
 	}
-	if (argc == 2)
-		close(fd);
+	printf("%s", line);
+	free(line);
+	close(fd1);
+	close(fd2);
 	return (0);
 }
